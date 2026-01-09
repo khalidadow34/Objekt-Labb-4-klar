7a9eb574e252d2b02a9b4784ebb66ec90abdd78a
khalidadow34 <khalidadow34@gmail.com>
2026-01-09 16:49:13 +0100

    Add files via upload

---
+++ Objekt Labb 4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+using static Objekt_Labb_4.Sprint_1;
+
+namespace Objekt_Labb_4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            List<Person> people;
+
+            people = new List<Person>();
+
+            bool Villkor = true;
+            bool Villkor2 = true;
+
+
+            Sprint_1.Person PushinP = new Sprint_1.Person
+            {
+                BirthDate = new DateTime(2001, 3, 13),
+                EyeColor = "Blue",
+                Gender = Sprint_1.Gender.Man,
+
+                Hair = new Sprint_1.Hair
+
+                {
+                    Length = 12.5,
+                    Color = "Brown",
+                    Type = "Curly"
+
+                }
+
+            };
+
+            people.Add(PushinP);
+
+
+            while (Villkor)
+            {
+
+
+
+                Console.WriteLine("Välj ett alternativ : \n" +
+                "1. Visa nuvarande Lista\n" +
+                "2. Lägg till person \n" +
+                "3. Avsluta Program");
+
+                string inputchoice = Console.ReadLine();
+
+                
+                switch (inputchoice)
+                {
+                    case "1":
+
+                        ListPersons(people);
+                        break;
+
+                    
+                    case "2":
+
+                        AddPerson(people);
+
+                        Console.WriteLine();
+                        Console.WriteLine("Kör Tillbaka Till Menyn");
+                        Console.WriteLine();
+                        break;
+
+
+                    case "3":
+
+                        Console.WriteLine("Avslutar Programmet...");
+
+                        Villkor = false;
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Fel inmatning. Försök igen");
+                        break;
+                }
+
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+    
+}
---
+++ Objekt Labb 4/Sprint 1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Objekt_Labb_4.Sprint_1;
+
+namespace Objekt_Labb_4
+{
+    internal class Sprint_1
+    {
+        public enum Gender
+        {
+            Man,
+            Kvinna,
+            IckeBinar,
+            Annan
+        }
+
+        public struct Hair
+        {
+            public double Length { get; set; }
+            public string Color { get; set; }
+            public string Type { get; set; }
+
+            public override string ToString()
+            {
+                return $"Length is: {Length}, Hair Color is: {Color}, Hair Type is: {Type}";
+            }
+        }
+
+        public class Person
+        {
+            public Gender Gender { get; set; }
+            public Hair Hair { get; set; }
+            public string EyeColor { get; set; }
+            public DateTime BirthDate { get; set; }
+
+            public override string ToString()
+            {
+                return $"Gender is {Gender}, Hair {Hair}, Eye Color is {EyeColor}, Birth Date is {BirthDate.ToShortDateString()}";
+
+            }
+        }
+
+        // sprint 2 efter 
+
+        public static void AddPerson(List<Person> people)
+        {
+
+            try
+            {
+                Console.WriteLine("Fyll in din Gender: ");
+                string inputGender = Console.ReadLine();
+
+                Console.WriteLine("Fyll in din Hair length: ");
+                double inputhairLength = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Fyll in din Hair Color: ");
+                string inputHairColor = Console.ReadLine();
+
+                Console.WriteLine("Fyll in din Hair Type: ");
+                string inputHairType = Console.ReadLine();
+
+                Console.WriteLine("Fyll in din Eye Color: ");
+                string inputEyeColor = Console.ReadLine();
+
+                Console.WriteLine("Fyll in din Birth Date (yyyy-mm-dd) : ");
+                DateTime inputBirthDate = DateTime.Parse(Console.ReadLine());
+
+
+
+
+                Person newPerson = new Person
+                {
+                    Gender = Enum.Parse<Gender>(inputGender, true),
+                    //Hair = inputhairLength,inputHair//
+
+                    EyeColor = inputEyeColor,
+                    BirthDate = inputBirthDate,
+
+                    Hair = new Hair()
+                    {
+                        Length = inputhairLength,
+                        Color = inputHairColor,
+                        Type = inputHairType
+                    }
+                };
+
+
+                people.Add(newPerson);
+
+                Console.WriteLine(newPerson.ToString());
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Fel inmatning, försök igen.");
+            }
+        }
+
+        public static void ListPersons(List<Person> people)
+        {
+            foreach (Person pList in people)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine(pList.ToString());
+
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
+
